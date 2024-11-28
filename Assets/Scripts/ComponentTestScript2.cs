dcf0dc7e0d00d53027c9a579614506a92796f90a
stuart <pkplonker@gmail.com>
2024-11-28 11:06:02 +0000

    Fixed some warnings and validated prop change

---
+++ Assets/Scripts/ComponentTestScript2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentTestScript : MonoBehaviour
+{
+    public float Number = 10f;
+
+    public string Word = "Testing123";
+
+    private void Update()
+    {
+        Debug.Log(Word);
+    }
+}
