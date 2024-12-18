5291aef3f0651a640c76c6ddc673873cd727624e
stuart <pkplonker@gmail.com>
2024-12-18 19:51:43 +0000

    Remove unneeded post process change

--- Assets/Editor/Processors/PropertyRTUEditorProcessor.cs
+++ Assets/Editor/Processors/PropertyRTUEditorProcessor.cs
@@ -49,34 +49,11 @@ namespace RTUEditor
 			{
 				foreach (var change in args)
 				{
-					if (PostProcessChange(change, out var postProcessChange))
-					{
-						controller.SendMessageToGame($"property,\n{JsonConvert.SerializeObject(postProcessChange)}");
-					}
-					else
-					{
-						var message =
-							$"property,\n{JsonConvert.SerializeObject(change, Formatting.Indented, new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore})}";
-						controller.SendMessageToGame(message);
-					}
-
-					// https://discussions.unity.com/t/jsonserializationexception-self-referencing-loop-detected/877513/8 to mitigate vector3 self looping w/ normalized
+					var message =
+						$"property,\n{JsonConvert.SerializeObject(change, Formatting.Indented, new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore})}";
+					controller.SendMessageToGame(message);
 				}
 			}
 		}
-
-		private bool PostProcessChange(PropertyChangeArgs change, out PropertyChangeArgs o)
-		{
-			o = change.Clone();
-			if (change.Value is UnityEngine.Vector3 v3)
-			{
-				o.Value = new Vector3Wrapper(v3);
-				return true;
-			}
-
-			return false;
-		}
 	}
-
-	
 }
