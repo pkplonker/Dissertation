41990fb9d6395be1bf8f609bb7e608af5962b3cd
stuart <pkplonker@gmail.com>
2024-12-27 19:58:53 +0000

    Converted hashing to support unity objects

---
+++ Assets/Scripts/RTU/JSONConverters/AssetJsonConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace RealTimeUpdateRuntime
+{
+	[JSONCustomConverter(typeof(GameObjectJsonConverter))]
+	public class GameObjectJsonConverter : JsonConverter<UnityEngine.GameObject>
+	{
+		public override void WriteJson(JsonWriter writer, GameObject value, JsonSerializer serializer)
+		{
+			if (value == null)
+			{
+				writer.WriteNull();
+				return;
+			}
+
+			writer.WriteStartObject();
+			writer.WritePropertyName("GameObjectPath");
+
+			writer.WriteValue(value.GetFullName() ?? string.Empty);
+
+			writer.WriteEndObject();
+		}
+
+		public override GameObject ReadJson(JsonReader reader, Type objectType, GameObject existingValue,
+			bool hasExistingValue,
+			JsonSerializer serializer)
+		{
+			throw new NotImplementedException("Deserialization is not implemented yet.");
+		}
+	}
+}
---
+++ Assets/Scripts/RTU/JSONConverters/ComponentJsonConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace RealTimeUpdateRuntime
+{
+	[JSONCustomConverter(typeof(GameObjectJsonConverter))]
+	public class GameObjectJsonConverter : JsonConverter<UnityEngine.GameObject>
+	{
+		public override void WriteJson(JsonWriter writer, GameObject value, JsonSerializer serializer)
+		{
+			writer.WriteStartObject();
+			writer.WritePropertyName("GameObjectPath");
+			writer.WriteValue(value.gameObject.GetFullName());
+			writer.WritePropertyName("ComponentType");
+			writer.WriteValue(value.GetType().Name);
+			writer.WriteEndObject();
+		}
+
+		public override GameObject ReadJson(JsonReader reader, Type objectType, GameObject existingValue,
+			bool hasExistingValue,
+			JsonSerializer serializer)
+		{
+			throw new NotImplementedException("Deserialization is not implemented yet.");
+		}
+	}
+}
