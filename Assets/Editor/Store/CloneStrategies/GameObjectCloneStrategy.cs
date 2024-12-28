a4be4e8d980cfffd3a74292977f3eedb6aa70d08
stuart <pkplonker@gmail.com>
2024-12-28 00:44:23 +0000

    Reverting async cloning to avoid attempting getinstanceid off main thread

--- Assets/Editor/Store/CloneStrategies/GameObjectCloneStrategy.cs
+++ Assets/Editor/Store/CloneStrategies/GameObjectCloneStrategy.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -14,22 +12,10 @@ namespace RTUEditor.AssetStore
 		{
 			if (asset is not GameObject go) return null;
 			var clone = CloneInternal(asset, asset.GetType(), new GameObjectClone(path)) as GameObjectClone;
-			var components = new ConcurrentBag<ComponentClone>();
-
-			Parallel.ForEach(go.GetComponents(typeof(Component)), component =>
-			{
-				var strategy = componentCloneStrategyFactory.GetCloneStrategy(component);
-				if (strategy != null)
-				{
-					var clonedComponent = strategy.Clone(component, component.GetType().ToString());
-					if (clonedComponent is ComponentClone componentClone)
-					{
-						components.Add(componentClone);
-					}
-				}
-			});
-
-			clone.components = components.ToList();
+			clone.components = go.GetComponents(typeof(Component))
+				.Select(x => componentCloneStrategyFactory.GetCloneStrategy(x)?.Clone(x, x.GetType().ToString()))
+				.OfType<ComponentClone>()
+				.ToList();
 			return clone;
 		}
 	}
